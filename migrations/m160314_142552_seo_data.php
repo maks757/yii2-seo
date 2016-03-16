608c5e374d4e748822a800758cd97f3d834a0f09
Вадим <guts.vadim@gmail.com>
2016-03-16 17:00:29 +0200

    seo_data an seo_meta_robots tables

---
+++ migrations/m160314_142552_seo_data.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+class m160314_142552_seo_data extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('{{%seo_data}}', [
+            'id' => $this->primaryKey(),
+            'entity_id' =>  $this->string(255)->notNull(),
+            'entity_name' => $this->string(255)->notNull(),
+            'seo_url' => $this->string(255),
+            'title' => $this->string(255),
+            'keywords' => $this->string(512),
+            'description' => $this->string(1024),
+            'meta_robots_id' => $this->integer()
+        ]);
+
+        $this->createIndex('seo_data_entity', '{{%seo_data}}', ['entity_id', 'entity_name'], true);
+
+        $this->createTable('{{%seo_meta_robots}}', [
+            'id' => $this->primaryKey(),
+            'value' => $this->string(255)
+        ]);
+        $this->insert('{{%seo_meta_robots}}', [
+            'value' => 'noindex,follow'
+        ]);
+        $this->insert('{{%seo_meta_robots}}', [
+            'value' => 'noindex,nofollow'
+        ]);
+
+        $this->addForeignKey(
+            'seo_data_meta_robots',
+            '{{%seo_data}}', 'meta_robots_id',
+            '{{%seo_meta_robots}}', 'id'
+        );
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%seo_data}}');
+        $this->dropTable('{{%seo_meta_robots}}');
+    }
+}
